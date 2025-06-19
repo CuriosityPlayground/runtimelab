9eb977bcb9f433649d2a4a0e15d5d1ea4746f218
Sebastian Schöner <mail@s-schoener.com>
2025-06-19 14:56:42 +0200

    Add required changes

--- src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/ObjectWriter/ObjectWriter.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/ObjectWriter/ObjectWriter.cs
@@ -418,18 +418,21 @@ namespace ILCompiler.ObjectWriter
                 long thumbBit = _nodeFactory.Target.Architecture == TargetArchitecture.ARM && isMethod ? 1 : 0;
                 foreach (ISymbolDefinitionNode n in nodeContents.DefinedSymbols)
                 {
+                    string mangledName = n == node ? currentSymbolName : GetMangledName(n);
+                    bool global = _nodeFactory.ShouldBeGlobalAndTrack(n, mangledName);
                     sectionWriter.EmitSymbolDefinition(
-                        n == node ? currentSymbolName : GetMangledName(n),
+                        mangledName,
                         n.Offset + thumbBit,
-                        n.Offset == 0 && isMethod ? nodeContents.Data.Length : 0);
+                        n.Offset == 0 && isMethod ? nodeContents.Data.Length : 0, global);
                     if (_nodeFactory.GetSymbolAlternateName(n, out bool isHidden) is string alternateName)
                     {
                         string alternateCName = ExternCName(alternateName);
+                        bool alternateGlobal = _nodeFactory.ShouldBeGlobalAndTrack(n, alternateCName, isHidden);
                         sectionWriter.EmitSymbolDefinition(
                             alternateCName,
                             n.Offset + thumbBit,
                             n.Offset == 0 && isMethod ? nodeContents.Data.Length : 0,
-                            global: !isHidden);
+                            global: alternateGlobal);
 
                         if (n is IMethodNode)
                         {

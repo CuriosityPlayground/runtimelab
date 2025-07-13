77f865503ce1e98a8ac7cc2c548abf1ca474ce40
Jonas Echterhoff <jonas@unity3d.com>
2025-07-13 21:35:43 +0200

    hack for detecting non-function exports

--- src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/DependencyAnalysis/Target_Wasm/WasmFunctionType.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/DependencyAnalysis/Target_Wasm/WasmFunctionType.cs
@@ -27,7 +27,8 @@ namespace ILCompiler.DependencyAnalysis.Wasm
             return hash.ToHashCode();
         }
 
-        public static bool IsFunction(ISymbolNode symbol) => symbol is ExternSymbolNode or IWasmFunctionNode or IMethodNode { Offset: 0 };
+        public static bool IsFunction(ISymbolNode symbol) => 
+            (symbol is ExternSymbolNode esn && !esn.Utf8Name.ToString().Contains("NonGCStaticBase") && !esn.Utf8Name.ToString().Contains("MethodTable")) || symbol is IWasmFunctionNode or IMethodNode { Offset: 0 };
 
         public static bool Equals(WasmFunctionType type, WasmValueType result, ReadOnlySpan<WasmValueType> parameters) =>
             type._result == result && type.Parameters.SequenceEqual(parameters);

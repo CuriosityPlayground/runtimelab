8a0666911baf87555f032f1c98d3bc7b89950f76
Jonas Echterhoff <jonas@unity3d.com>
2025-07-14 08:47:22 +0200

    fix merge

--- src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/DependencyAnalysis/ExternSymbolsImportedNodeProvider.cs
+++ src/coreclr/tools/aot/ILCompiler.Compiler/Compiler/DependencyAnalysis/ExternSymbolsImportedNodeProvider.cs
@@ -26,7 +26,15 @@ namespace ILCompiler.DependencyAnalysis
 
         public override ISortableSymbolNode ImportedNonGCStaticNode(NodeFactory factory, MetadataType type)
         {
-            return new ExternDataSymbolNode(NonGCStaticsNode.GetMangledName(type, factory.NameMangler));
+            string importPrefix = string.Empty;
+            if (_context.Target.IsWindows)
+            {
+                // On Windows, we need to explicitly refer to the exported data. We also need to explicitly mark the
+                // export as a DATA export, and in those cases you have to directly refer to the linked symbol, because
+                // the linker does not generate a thunk.
+                importPrefix = "__imp_";
+            }
+            return new ExternDataSymbolNode(importPrefix + NonGCStaticsNode.GetMangledName(type, factory.NameMangler));
         }
 
         public override ISortableSymbolNode ImportedMethodDictionaryNode(NodeFactory factory, MethodDesc method)
